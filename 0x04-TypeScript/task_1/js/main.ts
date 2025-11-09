ea5c122706ee54bd0baa0e8516096e0a519a3a94
Ugochukwu Ebifa <ebifaugo@gmail.com>
2025-11-09 23:34:27 +0100

    extend teacher class

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -33,3 +33,18 @@ const teacher3: Teacher = {
   location: "Lagos"
   // yearsOfExperience is optional, so this is fine
 };
+
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+
+console.log(director1);
